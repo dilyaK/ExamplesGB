afdb659c6b1c4767b69da47cd15f0bad3c76a89f
dilya <1dilnora777@gmail.com>
2022-11-06 04:30:51 +0500

    Ex006 Masha

---
+++ Example006_Masha/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter your name");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "jane")
+{
+    Console.WriteLine("Wow, it's Jane!");
+
+}
+
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
